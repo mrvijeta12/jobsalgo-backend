2288f407cf68ba29d197390bc2c4658100e96894
vijeta <vijetavarma1@gmail.com>
2025-11-06 07:45:59 +0530

    vercel part removed

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,52 +4,45 @@ import bcrypt from "bcryptjs";
 // register
 
 const registerUser = async (req, res) => {
-  if (req.method === "POST") {
-    try {
-      const { name, email, password } = req.body;
-      if (!name) {
-        return res.json({ message: "Name is required" });
-      }
-      if (!email) {
-        return res.json({ message: "Email is required" });
-      }
-      if (!password) {
-        return res.json({ message: "Password is required" });
-      }
-      if (password.length < 6) {
-        return res.json({
-          message: "Password length must me at least 6 characters",
-        });
-      }
-
-      const existUser = await User.findOne({ email });
-      if (existUser) {
-        return res.json({ message: "User is already register" });
-      }
-
-      const salt = await bcrypt.genSalt(10);
-      const hashPassword = await bcrypt.hash(password, salt);
-      const newUser = await User.create({
-        name,
-        email,
-        password: hashPassword,
-      });
-      res.status(201).json({
-        message: "User register successfully",
-        user: {
-          id: newUser._id,
-          name: newUser.name,
-          email: newUser.email,
-        },
-      });
-    } catch (error) {
-      res.status(501).json({ message: "Server error" });
+  try {
+    const { name, email, password } = req.body;
+    if (!name) {
+      return res.json({ message: "Name is required" });
     }
-  } else {
-    res.setHeader("Allow", ["POST"]);
-    return res
-      .status(405)
-      .json({ message: `Method ${req.method} not allowed` });
+    if (!email) {
+      return res.json({ message: "Email is required" });
+    }
+    if (!password) {
+      return res.json({ message: "Password is required" });
+    }
+    if (password.length < 6) {
+      return res.json({
+        message: "Password length must me at least 6 characters",
+      });
+    }
+
+    const existUser = await User.findOne({ email });
+    if (existUser) {
+      return res.json({ message: "User is already register" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashPassword,
+    });
+    res.status(201).json({
+      message: "User register successfully",
+      user: {
+        id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+      },
+    });
+  } catch (error) {
+    res.status(501).json({ message: "Server error" });
   }
 };
 
